a29548b4599ebb15fb15507c08e92ed50f70644b
Bharat Saiju <bsaiju@ucsb.edu>
2024-10-14 20:37:42 -0700

    team test for equals

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -18,9 +18,46 @@ public class TeamTest {
     public void getName_returns_correct_name() {
        assert(team.getName().equals("test-team"));
     }
-
    
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
+
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    @Test
+    public void equals_for_same_object() {
+        assertTrue(team.equals(team),"Equals fails on same object case");
+    }
+
+    @Test
+    public void equals_for_different_class() {
+        String other_team = "other-team";
+        assertTrue(!team.equals(other_team), "Equals fails on instances of different classes");
+    }
+
+    @Test
+    public void equals_for_name_and_members() {
+        Team other_team = new Team("test-team");
+        other_team.addMember("Godzilla");
+        team.addMember("Godzilla");
+        assertTrue(team.equals(other_team), "Equals fails on same name and same members");
+    }
+    @Test
+    public void equals_for_name_not_members() {
+        Team other_team = new Team("test-team");
+        other_team.addMember("Phelps");
+        assertTrue(!team.equals(other_team), "Equals fails on same name but different members");
+    }
+
+    @Test
+    public void equals_for_members_not_name() {
+        Team other_team = new Team("other-team");
+        other_team.addMember("Dracula");
+        team.addMember("Dracula");
+        assertTrue(!team.equals(other_team), "Equals fails on different name but same members");
+    }
 
 }
